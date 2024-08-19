07d8e4eee2f4a7b6565fd20206416946ffdf191e
CodeG52 <theonlycodegenius@gmail.com>
2024-08-19 11:47:53 +0300

    mastering

--- app/(tabs)/home/detail.jsx
+++ app/(tabs)/home/detail.jsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Button, StatusBar, View } from "react-native";
+import { Video } from "expo-av";
 
 const CourseDetails = () => {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
+  const videoStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    height: 250,
+  };
+
   return (
-    <View>
-      <Text>CourseDetails</Text>
+    <View style={{ flex: 1 }}>
+      <Video
+        source={{
+          uri: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        }}
+        ref={video}
+        style={videoStyle}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <View style={{ margin: 20 }}>
+        {/* <Button
+          title="Play from 5s"
+          onPress={() => video.current.playFromPositionAsync(5000)}
+        />
+        <Button
+          title={status.isLooping ? "Set to not loop" : "Set to loop"}
+          onPress={() => video.current.setIsLoopingAsync(!status.isLooping)}
+        /> */}
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 };
 
 export default CourseDetails;
-
-const styles = StyleSheet.create({});
